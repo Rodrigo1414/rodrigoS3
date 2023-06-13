dd5081a50f2fcbaadfdcacc8d78431e665c9c258
Administrador <Administrador@SGDPN-DTIC-SP>
2023-06-12 19:26:29 -0500

    Agregar archivos de proyecto.

---
+++ rodrigoS3/Page2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rodrigoS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string datoUno, int datoDos)
+        {
+            InitializeComponent();
+
+           
+        }
+    }
+}
---
+++ rodrigoS3/Page3.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rodrigoS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+        private void btnAbrirP_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
